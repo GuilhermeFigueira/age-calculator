d986f395d39a605cd0a7cc2c10ec6a6233b58a84
Guilherme Figueira <guibf2004@gmail.com>
2023-06-01 16:23:15 -0300

    varias coisas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`bg-off_white ${poppins.className} h-screen`}>
+			<body className={`bg-off_white ${poppins.className}`}>
 				{children}
 			</body>
 		</html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,131 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
 import Image from "next/image";
+import { useState } from "react";
 interface IFormInput {
 	day: number;
 	month: number;
 	year: number;
 }
 export default function Home() {
-	const { register, handleSubmit } = useForm<IFormInput>();
-	const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+	const current = new Date();
+	const [years, setYears] = useState<number>();
+	const [months, setMonths] = useState<number>();
+	const [days, setDays] = useState<number>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		watch,
+	} = useForm<IFormInput>({
+		defaultValues: {},
+	});
+
+	const onSubmit: SubmitHandler<IFormInput> = (data) => {
+		let ty = current.getFullYear();
+		let tm = current.getMonth() + 1;
+
+		if (current.getDate() < data.day) {
+			setDays(current.getDate() - data.day + 30);
+			tm = tm - 1;
+		} else {
+			setDays(current.getDate() - data.day);
+		}
+
+		if (tm < data.month) {
+			setMonths(tm - data.month + 12);
+			ty = ty - 1;
+		} else {
+			setMonths(tm - data.month);
+		}
+		setYears(ty - data.year);
+	};
+
 	return (
 		<main className="bg-white p-6 max-w-screen-md flex flex-col items-center rounded-3xl rounded-br-[6rem] mt-24">
 			<form
 				onSubmit={handleSubmit(onSubmit)}
-				className="grid grid-cols-3 gap-4 w-fit pb-10 border-b-2"
+				className="grid grid-cols-3 gap-4 w-fit mb-10 border-b-2"
 			>
-				<div className="input">
+				<div
+					className={`input${
+						errors.day?.message == undefined ? "" : "_error "
+					}`}
+				>
 					<label>Day</label>
 					<input
 						{...register("day", {
-							required: "Can't be blank",
+							required: "This field is required",
 							min: {
-								value: 0,
-								message: "Must be a valid day",
-							},
-							max: {
-								value: 32,
+								value: 1,
 								message: "Must be a valid day",
 							},
 						})}
-						placeholder="DD"
 					/>
+					<h1>{errors.day?.message}</h1>
 				</div>
-				<div className="input">
+				<div
+					className={`input${
+						errors.month?.message == undefined ? "" : "_error "
+					}`}
+				>
 					<label>Month</label>
-					<input {...register("month")} placeholder="MM" />
+					<input
+						{...register("month", {
+							required: "This field is required",
+							min: {
+								value: 1,
+								message: "Must be a valid month",
+							},
+							max: {
+								value: 12,
+								message: "Must be a valid month",
+							},
+						})}
+					/>
+					<h1>{errors.month?.message}</h1>
 				</div>
-				<div className="input">
+				<div
+					className={`input${
+						errors.year?.message == undefined ? "" : "_error "
+					}`}
+				>
 					<label>Year</label>
-					<input {...register("year")} placeholder="YY" />
+					<input
+						{...register("year", {
+							required: "This field is required",
+							min: {
+								value: 1850,
+								message: "Must be a valid year",
+							},
+							max: {
+								value: current.getFullYear(),
+								message: "Must be in the past",
+							},
+						})}
+					/>
+					<h1>{errors.year?.message}</h1>
 				</div>
+				<button className="flex rounded-full bg-purple hover:bg-off_black w-16 h-16 justify-center items-center justify-self-center translate-y-1/2 [grid-column:2/3]">
+					<Image
+						src="/icon-arrow.svg"
+						width={30}
+						height={30}
+						alt="Arrow pointing down"
+					/>
+				</button>
 			</form>
-			<button className="flex rounded-full bg-purple hover:bg-off_black w-16 h-16 justify-center items-center -translate-y-1/2">
-				<Image
-					src="/icon-arrow.svg"
-					width={30}
-					height={30}
-					alt="Arrow pointing down"
-				/>
-			</button>
-			<footer className="flex flex-col ">
+			<footer className="flex flex-col tracking-tighter">
 				<div className="results">
-					<strong>38</strong> years
+					<strong>{years == null ? "--" : years}</strong> years
 				</div>
 				<div className="results">
-					<strong>3</strong> months
+					<strong>{months == null ? "--" : months}</strong> months
 				</div>
 				<div className="results">
-					<strong>36</strong> days
+					<strong>{days == null ? "--" : days}</strong> days
 				</div>
 			</footer>
 		</main>
 	);
 }
-// 									<input
-// 											type="text"
-// 											{...register("name", {
-// 												required: "Can't be blank",
-// 												minLength: {
-// 													value: 3,
-// 													message:
-// 														"The minimum lenght is 3",
-// 												},
-// 												maxLength: {
-// 													value: 60,
-// 													message:
-// 														"The max lenght is 60",
-// 												},
-// 											})}
-// 											placeholder="e.g Jane Appleseed"
-// 											className="w-full"
-// 										/>
-// 									<span>{errors.name?.message}</span>
