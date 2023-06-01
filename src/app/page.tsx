7624ececa1f87d3715eb552e4173c60d8c286341
Guilherme Figueira <guibf2004@gmail.com>
2023-06-01 17:10:48 -0300

    quase completo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,71 +42,76 @@ export default function Home() {
 	};
 
 	return (
-		<main className="bg-white p-6 max-w-screen-md flex flex-col items-center rounded-3xl rounded-br-[6rem] mt-24">
+		<main className="bg-white p-6 max-w-screen-md flex flex-col items-center rounded-3xl rounded-br-[6rem] mt-24 m-auto md:mt-40 transition-all md:p-8">
 			<form
 				onSubmit={handleSubmit(onSubmit)}
-				className="grid grid-cols-3 gap-4 w-fit mb-10 border-b-2"
+				className="grid gap-4 w-full mb-10 border-b-2"
 			>
-				<div
-					className={`input${
-						errors.day?.message == undefined ? "" : "_error "
-					}`}
-				>
-					<label>Day</label>
-					<input
-						{...register("day", {
-							required: "This field is required",
-							min: {
-								value: 1,
-								message: "Must be a valid day",
-							},
-						})}
-					/>
-					<h1>{errors.day?.message}</h1>
+				<div className="grid grid-cols-3 w-fit gap-6 m-auto md:m-0 transition-all md:transition-all">
+					<div
+						className={`input${
+							errors.day?.message == undefined ? "" : "_error "
+						}`}
+					>
+						<label>Day</label>
+						<input
+							{...register("day", {
+								required: "This field is required",
+								min: {
+									value: 1,
+									message: "Must be a valid day",
+								},
+							})}
+							placeholder="DD"
+						/>
+						<h1>{errors.day?.message}</h1>
+					</div>
+					<div
+						className={`input${
+							errors.month?.message == undefined ? "" : "_error "
+						}`}
+					>
+						<label>Month</label>
+						<input
+							{...register("month", {
+								required: "This field is required",
+								min: {
+									value: 1,
+									message: "Must be a valid month",
+								},
+								max: {
+									value: 12,
+									message: "Must be a valid month",
+								},
+							})}
+							placeholder="MM"
+						/>
+						<h1>{errors.month?.message}</h1>
+					</div>
+					<div
+						className={`input${
+							errors.year?.message == undefined ? "" : "_error "
+						}`}
+					>
+						<label>Year</label>
+						<input
+							{...register("year", {
+								required: "This field is required",
+								min: {
+									value: 1850,
+									message: "Must be a valid year",
+								},
+								max: {
+									value: current.getFullYear(),
+									message: "Must be in the past",
+								},
+							})}
+							placeholder="YY"
+						/>
+						<h1>{errors.year?.message}</h1>
+					</div>
 				</div>
-				<div
-					className={`input${
-						errors.month?.message == undefined ? "" : "_error "
-					}`}
-				>
-					<label>Month</label>
-					<input
-						{...register("month", {
-							required: "This field is required",
-							min: {
-								value: 1,
-								message: "Must be a valid month",
-							},
-							max: {
-								value: 12,
-								message: "Must be a valid month",
-							},
-						})}
-					/>
-					<h1>{errors.month?.message}</h1>
-				</div>
-				<div
-					className={`input${
-						errors.year?.message == undefined ? "" : "_error "
-					}`}
-				>
-					<label>Year</label>
-					<input
-						{...register("year", {
-							required: "This field is required",
-							min: {
-								value: 1850,
-								message: "Must be a valid year",
-							},
-							max: {
-								value: current.getFullYear(),
-								message: "Must be in the past",
-							},
-						})}
-					/>
-					<h1>{errors.year?.message}</h1>
-				</div>
-				<button className="flex rounded-full bg-purple hover:bg-off_black w-16 h-16 justify-center items-center justify-self-center translate-y-1/2 [grid-column:2/3]">
+				<button className="flex rounded-full bg-purple hover:bg-off_black w-16 h-16 justify-center items-center justify-self-center  md:justify-self-end translate-y-1/2">
 					<Image
 						src="/icon-arrow.svg"
 						width={30}
